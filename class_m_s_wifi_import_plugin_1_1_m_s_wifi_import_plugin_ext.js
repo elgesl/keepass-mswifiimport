c81041e1072e9d6ac03ba9efa39c387a2dac34d3
Christopher R. Nerz <keepass@phoenixes.de>
2017-10-13 15:46:54 +0200

    Updated the documentation to include the newest version 0.2.0.2

--- class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.js
+++ class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.js
@@ -1,7 +1,6 @@
 var class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext =
 [
     [ "addToWindows", "class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.html#ac1788fe2715d49c1545fe0ae2fa79c76", null ],
-    [ "addToWindowsSep", "class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.html#a228fdd0279004c9416f69c6e8e631ac5", null ],
     [ "exportToXML", "class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.html#a3dea593c96f0826895c995dc056051e7", null ],
     [ "nameOfSelectedEntires", "class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.html#a348f0411e3617cb2af018b7d3a6f56c2", null ],
     [ "phHost", "class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.html#ac37b798bf5fd6e3f175f04e7fdef02cf", null ],
--- search/variables_0.js
+++ search/variables_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['addtowindows',['addToWindows',['../class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.html#ac1788fe2715d49c1545fe0ae2fa79c76',1,'MSWifiImportPlugin::MSWifiImportPluginExt']]],
-  ['addtowindowssep',['addToWindowsSep',['../class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.html#a228fdd0279004c9416f69c6e8e631ac5',1,'MSWifiImportPlugin::MSWifiImportPluginExt']]]
+  ['addtowindows',['addToWindows',['../class_m_s_wifi_import_plugin_1_1_m_s_wifi_import_plugin_ext.html#ac1788fe2715d49c1545fe0ae2fa79c76',1,'MSWifiImportPlugin::MSWifiImportPluginExt']]]
 ];
